1c662d41e9c67f25c66ef54e082e5c09e478dfd1
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-01-30 16:44:45 -0800

    install bootstrap and modify question box and header

--- quiz/src/components/QuestionBox.vue
+++ quiz/src/components/QuestionBox.vue
@@ -1,3 +1,15 @@
 <template>
-  <h1>QuestionBox</h1>
+  <div>
+    <b-jumbotron>
+      <template v-slot:lead>
+        Some question here?
+      </template>
+      <hr class="my-4" />
+      <p>
+        List of answers...
+      </p>
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button variant="success" href="#">Next</b-button>
+    </b-jumbotron>
+  </div>
 </template>
